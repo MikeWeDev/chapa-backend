973f4b377311798676c44ce3fea0380db2aad2af
MikeWeDev <mikyasnegash2@gmail.com>
2025-07-06 15:57:06 +0300

    removing chapa redirect url to redirect to the bot

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -48,7 +48,6 @@ router.post("/accept-payment", async (req, res) => {
         last_name,
         phone_number,
         tx_ref,
-       return_url: "https://chapa-payment-test.netlify.app/payment-success"
       },
       {
         headers: {
